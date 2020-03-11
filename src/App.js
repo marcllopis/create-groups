8bc989f622709b4ac30efa6d90e854f8da450997
Marc <marcllopis89@gmail.com>
2020-03-11 19:51:39 +0100

    Refactors App.js with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import AddStudentsForm from './components/AddStudentsForm/AddStudentsForm';
 import Button from './components/shared/Button';
@@ -9,77 +9,74 @@ import { shuffle } from './utils/functions';
 import './App.css';
 
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    students: [],
-    splittedGroups: [],
-    studentName: ""
-  }
+  let [students, setStudents] = useState([])
+  let [splittedGroups, setSplittedGroups] = useState([])
+  let [studentName, setStudentName] = useState("")
 
-  getFullClass = () => this.setState({ students: fullClass })
+  const getFullClass = () => setStudents(students = fullClass)
 
-  addStudent = e => this.setState({ studentName: e.target.value })
+  const addStudent = e => setStudentName(studentName = e.target.value)
 
-  getSingleStudent = e => {
+  const getSingleStudent = e => {
     e.preventDefault();
-    let studentsCopy = [...this.state.students]
-    studentsCopy.push(this.state.studentName)
-    this.setState({ students: studentsCopy, studentName: "" })
+    let studentsCopy = [...students]
+    studentsCopy.push(studentName)
+    setStudents(students = studentsCopy)
+    setStudentName(studentName = "")
   }
 
-  splitInGroups = e => {
-    let copyOfStudents = [...this.state.students];
+  const splitInGroups = e => {
+    let copyOfStudents = [...students];
     shuffle(copyOfStudents)
     let arrayHelper = [];
     while (copyOfStudents.length > 0) arrayHelper.push(copyOfStudents.splice(0, e.target.id))
-    this.setState({ splittedGroups: arrayHelper })
+    setSplittedGroups(splittedGroups = arrayHelper)
   }
 
-  render() {
-    return (
-      <div>
-        <div className="starting-form-container">
-          <div className="add-students-form">
-            <AddStudentsForm
-              getSingleStudent={this.getSingleStudent}
-              studentName={this.state.studentName}
-              addStudent={this.addStudent}
-            />
-          </div>
-          <div>
-            <Button
-              action={this.getFullClass}
-              buttonText={"Upload full class"}
-              className="add-button"
-            />
-          </div>
+  return (
+    <div>
+      <div className="starting-form-container">
+        <div className="add-students-form">
+          <AddStudentsForm
+            getSingleStudent={getSingleStudent}
+            studentName={studentName}
+            addStudent={addStudent}
+          />
         </div>
-        <div className="show-students">
-          {this.state.students.map(student => <p className="student-name">* {student}</p>)}
+        <div>
+          <Button
+            action={getFullClass}
+            buttonText={"Upload full class"}
+            className="add-button"
+          />
         </div>
-        {this.state.students.length > 0 &&
-          <div className="split-container">
-            <hr />
-            <div className="split-button-options">
-              <ButtonsGroups
-                splitInGroups={this.splitInGroups}
-                halfLength={this.state.students.length / 2}
-              />
-            </div>
-            <hr />
-            <div className="groups-displayed">
-              {this.state.splittedGroups.map(group =>
-                <p className="student-name">{group.map(person =>
-                  ` - ${person}`)}
-                </p>
-              )}
-            </div>
-          </div>
-        }
       </div>
-    )
-  }
+      <div className="show-students">
+        {students.map(student => <p className="student-name">* {student}</p>)}
+      </div>
+      {students.length > 0 &&
+        <div className="split-container">
+          <hr />
+          <div className="split-button-options">
+            <ButtonsGroups
+              splitInGroups={splitInGroups}
+              halfLength={students.length / 2}
+            />
+          </div>
+          <hr />
+          <div className="groups-displayed">
+            {splittedGroups.map(group =>
+              <p className="student-name">{group.map(person =>
+                ` - ${person}`)}
+              </p>
+            )}
+          </div>
+        </div>
+      }
+    </div>
+  )
 }
 
 
